8c8be4d89809ddaa646b2ed9e9d4a9d6ef89fa26
Hifnie Nazih <hifninazih111@gmail.com>
2025-05-03 05:46:18 +0700

    crud

--- peta/urls.py
+++ peta/urls.py
@@ -3,4 +3,13 @@ from . import views
 
 urlpatterns = [
     path("", views.LihatSuperHero, name="lihat_superhero"),
+    path("create/", views.createSuperHero, name="tambah_superhero"),
+    path("create/simpan/", views.simpanSuperHero, name="simpan"),
+    path("delete/<int:id>/", views.deleteSuperHero, name="delete"),
+    path("update/<int:id>/", views.updateSuperHero, name="update"),
+    path(
+        "update/simpan/<int:id>/",
+        views.simpanUpdateSuperHero,
+        name="simpan_update_superhero",
+    ),
 ]
--- peta/views.py
+++ peta/views.py
@@ -1,13 +1,13 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.shortcuts import redirect, render
+from django.contrib import messages
 from .models import Superhero
 import json
 
 
 def LihatSuperHero(request):
-    # Ambil data superhero dengan asosiasi 'Y'
-    # SemuaHero = Superhero.objects.filter(asosiasi="Y").values('nama', 'asosiasi', 'lat', 'long')
     SemuaHero = Superhero.objects.all().values(
-        "nama", "asosiasi", "lat", "long", "deskripsi"
+        "nama", "asosiasi", "lat", "long", "deskripsi", "id"
     )
     # Ubah queryset menjadi list of dictionaries dan konversi Decimal ke float
     SemuaHeroList = list(SemuaHero)
@@ -19,3 +19,45 @@ def LihatSuperHero(request):
     context = {"SemuaHero": json.dumps(SemuaHeroList)}
 
     return render(request, "peta/index.html", context)
+
+
+def createSuperHero(request):
+    return render(request, "peta/create.html")
+
+
+def simpanSuperHero(request):
+    superhero = Superhero()
+    superhero.nama = request.POST["nama"]
+    superhero.lat = request.POST["lat"]
+    superhero.long = request.POST["long"]
+    superhero.asosiasi = request.POST["asosiasi"]
+    superhero.deskripsi = request.POST["deskripsi"]
+    superhero.save()
+    messages.success(request, "Superhero berhasil disimpan")
+
+    return redirect("tambah_superhero")
+
+
+def deleteSuperHero(request, id):
+    hero = Superhero.objects.get(id=id)
+    hero.delete()
+    messages.success(request, "Superhero berhasil dihapus")
+    return redirect("/peta")
+
+
+def updateSuperHero(request, id):
+    hero = Superhero.objects.get(id=id)
+    context = {"hero": hero}
+    return render(request, "peta/update.html", context)
+
+
+def simpanUpdateSuperHero(request, id):
+    hero = Superhero.objects.get(id=id)
+    hero.nama = request.POST["nama"]
+    hero.lat = request.POST["lat"]
+    hero.long = request.POST["long"]
+    hero.asosiasi = request.POST["asosiasi"]
+    hero.deskripsi = request.POST["deskripsi"]
+    hero.save()
+    messages.success(request, "Superhero berhasil diupdate")
+    return redirect("/peta")
